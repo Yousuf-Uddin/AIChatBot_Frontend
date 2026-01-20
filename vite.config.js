823375a9b11102765e5b62b6f0f3e2fcad6172e0
[Mohd Yousuf] <yousuf.msoodi@gmail.com>
2026-01-20 22:23:27 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/LIET_AIChatBot/",
+  base: "/AIChatBot_Frontend/",
 });
